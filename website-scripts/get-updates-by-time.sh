dccd7ebe4b44bd4262bc51b27ef72b6ce71131e5
matteo <matt.simonetti@hotmail.it>
2023-10-14 15:53:35 +0200

    Update get-updates-by-time.sh

--- website-scripts/get-updates-by-time.sh
+++ website-scripts/get-updates-by-time.sh
@@ -1,10 +1,12 @@
-set -e
+set -ex
+UPDATES_DEST_PATH="../openstreetmap-tile-server/osm-updates/updates.osc.gz"
+OSC_FILE_NAME=update-`date +%m-%d-%Y-%H-%M`.osc.gz
 
-if [ $# -eq 2 ]; then
-	docker-compose run --rm web osmosis --read-apidb-change $(cat auth-file.txt) intervalBegin="$1" --wxc "$2"
-	exit 0
-elif [ $# -eq 3 ]; then
-	docker-compose run --rm web osmosis --read-apidb-change $(cat auth-file.txt) intervalBegin="$1" intervalEnd="$2" --wxc "$3"
-	exit 0
+if [ $# -eq 1 ]; then
+	docker-compose run --rm web osmosis --read-apidb-change $(cat auth-file.txt) intervalBegin="$1" --wxc "$OSC_FILE_NAME"
+elif [ $# -eq 2 ]; then
+	docker-compose run --rm web osmosis --read-apidb-change $(cat auth-file.txt) intervalBegin="$1" intervalEnd="$2" --wxc "$OSC_FILE_NAME"
 fi
-exit 1
+
+cp -i $OSC_FILE_NAME $UPDATES_DEST_PATH
+exit 0

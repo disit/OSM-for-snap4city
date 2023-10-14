3553d56e6cc0bbffbe4677eb76027e8fca4c035b
matteo <matt.simonetti@hotmail.it>
2023-10-14 15:41:38 +0200

    Updated get-updates-by-time.sh

--- website-scripts/get-updates-by-time.sh
+++ website-scripts/get-updates-by-time.sh
@@ -1,1 +1,10 @@
-docker-compose run --rm web osmosis --read-apidb-change $(cat auth-file.txt) intervalBegin=$1 --wxc $2
+set -e
+
+if [ $# -eq 2 ]; then
+	docker-compose run --rm web osmosis --read-apidb-change $(cat auth-file.txt) intervalBegin="$1" --wxc "$2"
+	exit 0
+elif [ $# -eq 3 ]; then
+	docker-compose run --rm web osmosis --read-apidb-change $(cat auth-file.txt) intervalBegin="$1" intervalEnd="$2" --wxc "$3"
+	exit 0
+fi
+exit 1

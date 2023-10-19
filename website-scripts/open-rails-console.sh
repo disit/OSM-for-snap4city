66c56bfa1a2a7cff38b90494b8507dc52eb03641
SimoMett <matt.simonetti@hotmail.it>
2023-10-19 16:29:43 +0200

    Update open-rails-console.sh

--- website-scripts/open-rails-console.sh
+++ website-scripts/open-rails-console.sh
@@ -1,1 +1,8 @@
+echo "###############"
+echo "Comandi utili:"
+echo "  user = User.find_by(:display_name => \"User Name\")"
+echo "  user.activate!"
+echo "  user.roles.create(:role => \"administrator\", :granter_id => user.id)"
+echo "  user.save!"
+echo "###############"
 docker-compose run --rm web bundle exec rails console

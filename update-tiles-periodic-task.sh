319e93ebfb0efa5a096400731a8d98d95e533c20
SimoMett <matt.simonetti@hotmail.it>
2023-10-19 11:45:41 +0200

    Updated periodic task

--- update-tiles-periodic-task.sh
+++ update-tiles-periodic-task.sh
@@ -3,13 +3,16 @@ UPDATES_DEST_PATH="openstreetmap-tile-server/osm-updates/updates.osc.gz"
 OSC_FILE_NAME=update-`date +%m-%d-%Y-%H-%M`.osc.gz
 
 cd openstreetmap-website
-if [ $# -eq 1 ]; then
-	docker-compose run --rm web osmosis --read-apidb-change $(cat auth-file.txt) intervalBegin="$1" --wxc "$OSC_FILE_NAME"
-elif [ $# -eq 2 ]; then
-	docker-compose run --rm web osmosis --read-apidb-change $(cat auth-file.txt) intervalBegin="$1" intervalEnd="$2" --wxc "$OSC_FILE_NAME"
+if test -f "last-update.txt"; then
+	docker-compose run --rm web osmosis --read-apidb-change $(cat auth-file.txt) intervalBegin="$(cat last-update.txt)" --wxc "$OSC_FILE_NAME"
+else
+	echo "last-update.txt non esiste"
+	exit 1
 fi
-
 cp -f $OSC_FILE_NAME $UPDATES_DEST_PATH
 cd ../openstreetmap-tile-server
 ./launch-update-task.sh
+cd ..
+rm last-update.txt
+echo "$(date +%Y-%m-%d_%H-%M-%S)" >> last-update.txt
 exit 0

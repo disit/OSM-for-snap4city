aeaa9776e40049b1471a7fd0faeb2ee7e00004ef
SimoMett <matt.simonetti@hotmail.it>
2023-11-22 14:15:54 +0100

    Update osm-tile-server-setup.sh

--- osm-tile-server-setup.sh
+++ osm-tile-server-setup.sh
@@ -2,8 +2,8 @@ echo "Cloning Github repo"
 set -e
 git clone https://github.com/SimoMett/openstreetmap-tile-server.git
 docker volume create osm-data && docker volume create osm-tiles
+mkdir openstreetmap-tile-server/osm-updates
+docker-compose build map
 cd openstreetmap-tile-server
-mkdir osm-updates
-docker-compose build
 chmod +x import-pbf.sh
 ./import-pbf.sh $(realpath ../Firenze.osm.pbf)

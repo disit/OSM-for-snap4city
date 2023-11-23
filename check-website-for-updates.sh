79f1da8912b2f34b00a6ca44d6f909c7d86d99a4
SimoMett <matt.simonetti@hotmail.it>
2023-11-23 10:57:43 +0100

    Cronified update script

--- check-website-for-updates.sh
+++ check-website-for-updates.sh
@@ -1,13 +1,15 @@
 set -e
-OSC_FILE=update.osc.gz
+OSC_FILE=cron-update.osc.gz
+OSM_SNAP4CITY_PATH="/home/matteo/Documenti/GitHub/OSM-for-snap4city/"
+cd $OSM_SNAP4CITY_PATH
 docker-compose run --rm web osmosis --read-apidb-change $(cat openstreetmap-website/auth-file.txt) intervalBegin=$(cat openstreetmap-tile-server/last-update.txt) readFullHistory=yes --wxc $OSC_FILE
 WAYS_COUNT=$(osmium fileinfo -e -g data.count.ways openstreetmap-website/$OSC_FILE)
 NODES_COUNT=$(osmium fileinfo -e -g data.count.nodes openstreetmap-website/$OSC_FILE)
 RELATIONS_COUNT=$(osmium fileinfo -e -g data.count.relations openstreetmap-website/$OSC_FILE)
 CHANGESETS_COUNT=$(osmium fileinfo -e -g data.count.changesets openstreetmap-website/$OSC_FILE)
 if [[ $WAYS_COUNT == 0 && $NODES_COUNT == 0 && $RELATIONS_COUNT == 0 && $CHANGESETS_COUNT == 0 ]] ; then
-	echo Nessun aggiornamento
-	rm -f $OSC_FILE
+	echo "Nessun aggiornamento disponibile"
 else
-	echo TODO aggiornamento
-fi 
+	echo "Aggiornamento disponibile!"
+fi
+rm -f openstreetmap-website/$OSC_FILE

89e041fde35e73227670e92f37359a02cc8d8720
SimoMett <matt.simonetti@hotmail.it>
2024-01-03 15:24:24 +0100

    Fixed already existing directory error

--- osm-tile-server-setup.sh
+++ osm-tile-server-setup.sh
@@ -5,7 +5,7 @@ fi
 
 docker volume create osm-data && docker volume create osm-tiles
 docker volume create osm-website_web-tmp && docker volume create osm-website_web-storage && docker volume create osm-website_db-data
-mkdir openstreetmap-tile-server/osm-updates
+mkdir -p openstreetmap-tile-server/osm-updates
 docker-compose build map
 cd openstreetmap-tile-server
 chmod +x import-pbf.sh
